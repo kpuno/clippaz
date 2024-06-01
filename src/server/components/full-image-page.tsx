f091db921215dabf9538a76d1038a1352b4d0ccc
Kyle Puno <kylepuno@outlook.com>
2024-06-01 14:06:55 -0400

    updated styles on image modal

--- src/server/components/full-image-page.tsx
+++ src/server/components/full-image-page.tsx
@@ -7,6 +7,14 @@ export default async function FullPageImageView(props: { photoId: number }) {
   const image = await getImage(idAsNumber)
 
   return (
-    <img src={image.url} alt={image.name} className="w-96" />
+    <div className="flex w-full min-w-0 h-full">
+      <div className="flex-shrink flex justify-center items-center">
+        <img src={image.url} alt={image.name} className="flex-shrink object-contain" />
+      </div>
+
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl">{image.name}</div>
+      </div>
+    </div>
   )
 }
