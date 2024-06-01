06699606f5712b400923bf488ed32f47fe8d3ee8
Kyle Puno <kylepuno@outlook.com>
2024-06-01 12:40:15 -0400

    break queries into separate file, updated fe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,37 @@
-import { db } from "~/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic"
 
+async function Images() {
+  const images = await getMyImages()
+
+  return (
+    <div className="flex flex-wrap gap-4" >
+      {
+        images.map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48 flex flex-col">
+            <img src={image.url} alt={image.name} />
+            <div>{image.name}</div>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
 export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id)
-  })
 
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {
-          images.map((image, index) => (
-            <div key={image.id + "-" + index} className="w-48 flex flex-col">
-              <img src={image.url} alt={image.name} />
-              <div>{image.name}</div>
-            </div>
-          ))
-        }
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text=2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
---
+++ src/server/queries.ts
@@ -0,0 +1,18 @@
+import { auth } from "@clerk/nextjs/server"
+import "server-only"
+import { db } from "~/server/db"
+
+export const getMyImages = async () => {
+  const user = auth()
+
+  if (!user.userId) throw new Error("Unauthorized")
+
+
+  const images = await db.query.images.findMany({
+    where: (model, { eq }) => eq(model.userId, user.userId),
+    orderBy: (model, { desc }) => desc(model.id)
+  })
+
+  return images
+}
+
